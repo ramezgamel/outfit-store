70e97903dbaef00bcd6cdd44a53dd46c26128e3a
ramez <ramezgamel52@gmail.com>
2025-01-19 10:19:40 +0200

    split auth file to reduce middle-ware size

--- auth.config.ts
+++ auth.config.ts
@@ -37,74 +37,74 @@ export const nextConfig = {
       },
     }),
   ],
-  // callbacks: {
-  //   async session({ session, user, trigger, token }: any) {
-  //     session.user.id = token.sub;
-  //     session.user.role = token.role;
-  //     session.user.name = token.name;
+  callbacks: {
+    async session({ session, user, trigger, token }: any) {
+      session.user.id = token.sub;
+      session.user.role = token.role;
+      session.user.name = token.name;
 
-  //     if (trigger == "update") {
-  //       session.user.name = user.name;
-  //     }
-  //     return session;
-  //   },
-  //   async jwt({ token, user, trigger }: any) {
-  //     if (user) {
-  //       token.role = user.role;
-  //       if (user.name === "NO_NAME") {
-  //         token.name = user.email.split("@")[0];
-  //         await prisma.user.update({
-  //           where: {
-  //             id: user.id,
-  //           },
-  //           data: { name: token.name },
-  //         });
-  //       }
-  //     }
-  //     if (trigger === "signIn" || trigger === "signUp") {
-  //       const sessionCartId = (await cookies()).get("sessionCartId")?.value;
-  //       if (sessionCartId) {
-  //         const sessionCart = await prisma.cart.findFirst({
-  //           where: { sessionCartId },
-  //         });
+      if (trigger == "update") {
+        session.user.name = user.name;
+      }
+      return session;
+    },
+    async jwt({ token, user, trigger }: any) {
+      if (user) {
+        token.role = user.role;
+        if (user.name === "NO_NAME") {
+          token.name = user.email.split("@")[0];
+          await prisma.user.update({
+            where: {
+              id: user.id,
+            },
+            data: { name: token.name },
+          });
+        }
+      }
+      if (trigger === "signIn" || trigger === "signUp") {
+        const sessionCartId = (await cookies()).get("sessionCartId")?.value;
+        if (sessionCartId) {
+          const sessionCart = await prisma.cart.findFirst({
+            where: { sessionCartId },
+          });
 
-  //         if (sessionCart) {
-  //           await prisma.cart.deleteMany({ where: { userId: user.id } });
+          if (sessionCart) {
+            await prisma.cart.deleteMany({ where: { userId: user.id } });
 
-  //           await prisma.cart.update({
-  //             where: { id: sessionCart.id },
-  //             data: { userId: user.id },
-  //           });
-  //         }
-  //       }
-  //     }
-  //     return token;
-  //   },
-  //   authorized({ request }: { request: NextRequest; auth: any }) {
-  //     const protectedPaths = [
-  //       /\/shipping-address/,
-  //       /\/payment-method/,
-  //       /\/place-order/,
-  //       /\/profile/,
-  //       /\/user\/(.*)/,
-  //       /\/order\/(.*)/,
-  //       /\/admin/,
-  //     ];
-  //     const { pathname } = request.nextUrl;
-  //     if (!auth && protectedPaths.some((p) => p.test(pathname))) return false;
-  //     if (!request.cookies.get("sessionCartId")) {
-  //       const sessionCartId = crypto.randomUUID();
-  //       const newRequestHeaders = request.headers;
-  //       const res = NextResponse.next({
-  //         request: {
-  //           headers: newRequestHeaders,
-  //         },
-  //       });
-  //       res.cookies.set("sessionCartId", sessionCartId);
-  //       return res;
-  //     } else {
-  //       return true;
-  //     }
-  //   },
-  // },
+            await prisma.cart.update({
+              where: { id: sessionCart.id },
+              data: { userId: user.id },
+            });
+          }
+        }
+      }
+      return token;
+    },
+    authorized({ request }: { request: NextRequest; auth: any }) {
+      const protectedPaths = [
+        /\/shipping-address/,
+        /\/payment-method/,
+        /\/place-order/,
+        /\/profile/,
+        /\/user\/(.*)/,
+        /\/order\/(.*)/,
+        /\/admin/,
+      ];
+      const { pathname } = request.nextUrl;
+      if (!auth && protectedPaths.some((p) => p.test(pathname))) return false;
+      if (!request.cookies.get("sessionCartId")) {
+        const sessionCartId = crypto.randomUUID();
+        const newRequestHeaders = request.headers;
+        const res = NextResponse.next({
+          request: {
+            headers: newRequestHeaders,
+          },
+        });
+        res.cookies.set("sessionCartId", sessionCartId);
+        return res;
+      } else {
+        return true;
+      }
+    },
+  },
 };
